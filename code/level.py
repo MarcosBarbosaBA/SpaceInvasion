6613fe462e7b22c23d6a0bb4bdd8b6f70ad8c5ab
MarcosBarbosaBA <145401819+MarcosBarbosaBA@users.noreply.github.com>
2025-03-29 15:15:34 -0300

    Music Volume adjusted

--- code/level.py
+++ code/level.py
@@ -30,6 +30,7 @@ class Level:
 
     def run(self, ):
         pygame.mixer.music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer.music.set_volume(0.3)
         pygame.mixer.music.play(-1)
         clock = pygame.time.Clock()
         while True:
